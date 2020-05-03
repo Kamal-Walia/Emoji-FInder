071f4126d8b5f58f9633a5a02d4a8ed219eb014d
Kamal <theoriginalkamal@gmail.com>
2020-05-03 14:43:16 +0530

    Updates

---
+++ precache-manifest.9ceec1ab686a16fa5bcb82e23f8261ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996e17dd33c12de867ee44f79ef8b306",
+    "url": "/Emoji-Finder/index.html"
+  },
+  {
+    "revision": "34a31ac47f4eda784cb4",
+    "url": "/Emoji-Finder/static/css/main.cad33c6f.chunk.css"
+  },
+  {
+    "revision": "1c1e7f653243d2575ae0",
+    "url": "/Emoji-Finder/static/js/2.b5a6d9a3.chunk.js"
+  },
+  {
+    "revision": "16192252175dc8f56422d4c21b326a4d",
+    "url": "/Emoji-Finder/static/js/2.b5a6d9a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34a31ac47f4eda784cb4",
+    "url": "/Emoji-Finder/static/js/main.021fb72e.chunk.js"
+  },
+  {
+    "revision": "5631e6538d363aa036a4",
+    "url": "/Emoji-Finder/static/js/runtime-main.d16e67ae.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Emoji-Finder/precache-manifest.6fb989af6ebccb35105819eb5e9198df.js"
+  "/Emoji-Finder/precache-manifest.9ceec1ab686a16fa5bcb82e23f8261ee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Emoji-Finder/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
